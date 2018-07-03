4fa4f57c018ad4cd88eb14a646fadc6588f9a4cc
WalterMccan <40248730+WalterMccan@users.noreply.github.com>
2018-07-03 22:48:34 +0100

    Create image2webp.py

---
+++ image2webp.py
@@ -0,0 +1,7 @@
+from PIL import Image
+import glob, os
+
+for f in glob.glob("*.jpg"):
+    file, extension = os.path.splitext(f)
+    im = Image.open(f).convert("RGB")
+    im.save(file + ".webp", lossless=True, exif=False)
